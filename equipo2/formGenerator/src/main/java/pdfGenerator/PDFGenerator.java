d1045ca99be5a8880dd1d76796d369dc2bbede14
dvidCR <dvidcr100@gmail.com>
2025-01-14 20:41:56 +0100

    equipo2 db

--- equipo2/formGenerator/src/main/java/pdfGenerator/PDFGenerator.java
+++ equipo2/formGenerator/src/main/java/pdfGenerator/PDFGenerator.java
@@ -1,31 +1,55 @@
 package pdfGenerator;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+
+import databaseAdmin.DatabaseGenerator;
+import databaseAdmin.DatabaseQuery;
 
 public class PDFGenerator {
-	public static void main(String[] args) {
-        String dest = "example.pdf";
+	
+	public static void main(String[] args) throws SQLException {
+		String pdfName = "Reporte_Motel.pdf";
+        DatabaseGenerator dbName = getBBDDName();
+        getQuery(dbName);
+        
 
         try {
-            // Crear un escritor de PDF
-            PdfWriter writer = new PdfWriter(dest);
-
-            // Crear un documento PDF
+            PdfWriter writer = new PdfWriter(pdfName);
             PdfDocument pdf = new PdfDocument(writer);
             Document document = new Document(pdf);
 
-            // Añadir un párrafo al documento
-            document.add(new Paragraph("¡Hola, este es un PDF generado en Java!"));
+            document.add(new Paragraph("Reporte de reservas Motel"));
 
-            // Cerrar el documento
+            generateTables(document);
+            
             document.close();
-
-            System.out.println("PDF creado en: " + dest);
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+	private static void getQuery(DatabaseGenerator dbUrl) {
+		DatabaseQuery dq = new DatabaseQuery(dbUrl);
+	}
+
+	public static void generateTables(Document document) {
+		Table table = new Table(3);
+		table.addCell("Columna 1");
+		table.addCell("Columna 2");
+		table.addCell("Columna 3");
+		document.add(table);
+	}
+	
+	public static DatabaseGenerator getBBDDName() {
+		DatabaseGenerator dg = new DatabaseGenerator();
+		return dg;
+	}
 }
