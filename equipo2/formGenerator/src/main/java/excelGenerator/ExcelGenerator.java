69e5ba8a92b83104e8ca103cbc11c2a4690ca8a3
dav200435 <davacas2005@gmail.com>
2025-01-14 20:16:09 +0100

    dataClases grupo2

--- equipo2/formGenerator/src/main/java/excelGenerator/ExcelGenerator.java
+++ equipo2/formGenerator/src/main/java/excelGenerator/ExcelGenerator.java
@@ -1,5 +1,26 @@
 package excelGenerator;
 
 public class ExcelGenerator {
-
+	private final String filePath;
+	private String fileName;
+	
+	public ExcelGenerator(String fileName) {
+		this.filePath = "./src/main/resuorces";
+		this.fileName = fileName;
+	}
+	
+	
+	public void generateExcel() {
+		String[] columns = {"ID", "Nombre", "Apellido", "Edad"};
+        Object[][] data = {
+            
+        };
+		
+	}
+	
+	private void writeExcel() {
+		
+	}
+	
+	
 }
