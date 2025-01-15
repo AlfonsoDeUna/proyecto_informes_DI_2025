b8168d40c035b4c8f4d3d7b2cb29198eec9811d3
dav200435 <davacas2005@gmail.com>
2025-01-15 20:36:55 +0100

    socket

--- equipo2/formGenerator/src/main/java/socketClient/SocketClient.java
+++ equipo2/formGenerator/src/main/java/socketClient/SocketClient.java
@@ -23,18 +23,17 @@ public class SocketClient {
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
 
-            // Recibimos el mensaje del servidor (menú)
             String serverMessage = input.readLine();
-            System.out.println("Servidor: " + serverMessage);
+            if (serverMessage != null) {
+                System.out.println("Servidor: " + serverMessage);
+            }
 
             Scanner sc = new Scanner(System.in);
             System.out.print("Ingresa un número para elegir una opción del menú: ");
             String userInput = sc.nextLine();
 
-            // Enviamos el número al servidor
             output.println(userInput);
 
-            // Recibimos los resultados del servidor
             String queryResult;
             while ((queryResult = input.readLine()) != null) {
                 queryResults.add(queryResult);
